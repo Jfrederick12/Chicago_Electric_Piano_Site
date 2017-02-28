54c3d104cbde1c0bcc08bd3320d9e659619da6ef
Jfrederick12 <jfrederickdesign@gmail.com>
2017-02-28 14:39:36 -0600

    fix issue with transparency on logo

--- app/assets/javascripts/components/_banner.js.jsx
+++ app/assets/javascripts/components/_banner.js.jsx
@@ -2,7 +2,7 @@ const Banner = React.createClass({
   render() {
     return(
       <div className="banner">
-        <img src="https://github.com/Jfrederick12/Chicago_Electric_Piano_Site/blob/master/app/assets/images/cepco_logo1.png?raw=true" height="50%" width="50%" />
+        <img src="https://github.com/Jfrederick12/Chicago_Electric_Piano_Site/blob/master/app/assets/images/cepco_logo.png?raw=true" height="50%" width="50%" />
       </div>
     )
   }
