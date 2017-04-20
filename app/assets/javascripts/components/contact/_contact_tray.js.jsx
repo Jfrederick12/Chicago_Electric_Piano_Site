d70adc7402668de9c3e40d813ca218853f11ed65
Justin Frederick <jfrederickdesign@gmail.com>
2017-04-20 16:52:48 -0500

    add working reverb link

--- app/assets/javascripts/components/contact/_contact_tray.js.jsx
+++ app/assets/javascripts/components/contact/_contact_tray.js.jsx
@@ -14,7 +14,7 @@ const ContactTray = React.createClass({
 				    	<p>The Chicago Electric Piano Co. is currently operating out of a warehouse at 3600 W Potomac (See mailing address for package receiving). Since the space does not have a door that opens directly to the street, The Chicago Electric Piano Co. works by appointment only. Our schedule is extremely flexible and appointments are available 8:00am-10:00pm Monday-Sunday.</p>
 				    	<p>Loading Instructions: When you arrive at the warehouse you will see a gated parking lot at the far west end of the building adjacent to the alley. Please call us a call at (312)476-9528 and we will come out to open the gate for you to pull right up to our front door.</p>
 				    	<h3>Check our Reverb shop</h3>
-				    	<a href='#' className="social-icon"> <i className='fa fa-twitter fa-2x'></i></a>
+				    	<a href="https://reverb.com/shop/chicagoelectricpiano"><img src="https://github.com/Jfrederick12/Chicago_Electric_Piano_Site/blob/blog-markdown/app/assets/images/image.png?raw=true" width="15%"/></a>
 			    	</Col>
 			    	<Col xs={6} md={6}>
 			    	  < ReactForm />
